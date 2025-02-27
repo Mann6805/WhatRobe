9d6c7f82dd12c7047746ec0ec33e1a41b0f122d1
Mannn <mannpatel060805@gmail.com>
2025-02-27 22:12:08 +0530

    Updated by mann

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:fashion_organiser/provider/userprovider.dart';
 import 'package:fashion_organiser/splashscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    
+  await Firebase.initializeApp();
+  // Activate App Check (using Debug mode for development)
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug, // Use PlayIntegrity for production
   );  
   Gemini.init(apiKey: "AIzaSyBdITjGKPyfRX1yIMRBsnYVq8tbCWku97s");
   runApp(ChangeNotifierProvider(
